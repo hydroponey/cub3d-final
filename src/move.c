ad6256669712136e378035079c2fae35cde8431d
asimoes <asimoes@student.42.fr>
2021-05-27 17:01:17 +0200

    Cleanup move.c

--- src/move.c
+++ src/move.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/20 04:54:04 by asimoes           #+#    #+#             */
-/*   Updated: 2021/05/22 23:31:59 by asimoes          ###   ########.fr       */
+/*   Updated: 2021/05/27 16:59:52 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,10 +47,10 @@ int	move_left(t_conf *conf)
 	if (conf->key_a)
 	{
 		if (conf->map[(int)(conf->posx - conf->diry
-				* (conf->move_speed * 2))][(int)conf->posy] == 0)
+				* conf->move_speed)][(int)conf->posy] == 0)
 			conf->posx -= conf->diry * conf->move_speed;
 		if (conf->map[(int)conf->posx][(int)(conf->posy
-			+ conf->dirx * (conf->move_speed * 2))] == 0)
+			+ conf->dirx * conf->move_speed * 2)] == 0)
 			conf->posy += conf->dirx * conf->move_speed;
 		return (1);
 	}
@@ -62,10 +62,10 @@ int	move_right(t_conf *conf)
 	if (conf->key_d)
 	{
 		if (conf->map[(int)(conf->posx + conf->diry
-				* (conf->move_speed))][(int)conf->posy] == 0)
+				* conf->move_speed)][(int)conf->posy] == 0)
 			conf->posx += conf->diry * conf->move_speed;
 		if (conf->map[(int)conf->posx][(int)(conf->posy
-			- conf->dirx * (conf->move_speed))] == 0)
+			- conf->dirx * conf->move_speed)] == 0)
 			conf->posy -= conf->dirx * conf->move_speed;
 		return (1);
 	}
